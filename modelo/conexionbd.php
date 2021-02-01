310c6c97423e1dd31d25647d838434641d94c1ed
sairy7 <71484435+sairy7@users.noreply.github.com>
2021-02-01 01:29:55 -0600

    actualizacion en el modulo de reservaciones
    actualizaciones corresponden a las pantallas de camping y hotel

---
+++ modelo/conexionbd.php
@@ -0,0 +1,27 @@
+<?php
+
+/*$server = 'f-a-aws.cqcxwtscprfd.us-east-1.rds.amazonaws.com';
+$user_name = 'root';
+$pass = 'Unah_fernando';
+$data_base = 'bd_fundacion_amitigra';
+$puerto = '3308';*/
+
+/*try {
+$conn = new mysqli($server, $user_name, $pass, $data_base, $puerto);
+$conn->set_charset('utf8');
+} catch (mysqlException $e){
+    die('Hubo un error con la conexion a la base de datos: '. $e->getMessage());
+}*/
+
+$server = 'localhost';
+$user_name = 'root';
+$pass = '';
+$data_base = 'bd_fundacion_amitigra';
+
+try{
+    $conn = new mysqli($server, $user_name, $pass, $data_base);
+    $conn->set_charset('utf8');
+
+} catch (mysqlException $e){
+    die('Se produjó un error en la conexión'. $e->getMessage());
+}
--- vista/modulos/hotel.php
+++ vista/modulos/hotel.php
@@ -1,38 +1,82 @@
-<?php
-include_once('funciones/sesiones.php');
-$usuario = $_SESSION['usuario'];
-?>
 
-<main>
+  <!-- =============================================== -->
+  <!-- Content Wrapper. Contains page content -->
+  <div class="content-wrapper">
 
-<div class="content-wrapper">
-    <!-- Main content -->
-    <section class="content">
+          <div class="box">
+            <div class="box-header">
+              <h3 class="box-title fa fa-bed"> Listado de reservaciones Hotel</h3>
+            </div>
+            <!-- /.box-header -->
+            <div class="box-body">
+              <a href="reservahotel" class=" btn bg-blue btn-flat ">Nueva Reservación <i class="fa fa-bed"></i></i></a>
+              <table id="reservacion" class="table table-bordered table-striped">
+                <thead>
+                <tr>
+                  <th>Cliente</th>
+                  <th>Fecha reservacion</th>
+                  <th>Fecha entrada</th>
+                  <th>Fecha salida</th>
+                  <th>accion</th>
+                </tr>
+                </thead>
+                <tbody>
+                <?php
+                  include_once ('./modelo/conexionbd.php');
+                try{
+                  $sql = "SELECT id_reservacion, fecha_reservacion, fecha_entrada,fecha_salida,tbl_clientes.nombre_completo ";  
+                  $sql .= " FROM tbl_reservaciones ";
+                  $sql .= " INNER JOIN tbl_clientes ";
+                  $sql .= " ON tbl_reservaciones.cliente_id=tbl_clientes.id_cliente ";
+                  $sql .= " ORDER BY id_reservacion ";
+                  $resultado = $conn->query($sql);
+                }catch (Exeption $e){
+                  $error = $e->getMessage();
+                  echo $error;
+                }
+                while($mostrar = $resultado->fetch_assoc()){ ?>
+                <tr>
+                  <td><?php echo $mostrar['nombre_completo'];?></td>
+                  <td><?php echo $mostrar['fecha_reservacion'];?></td>
+                  <td><?php echo $mostrar['fecha_entrada'];?></td>
+                  <td><?php echo $mostrar['fecha_salida'];?></td>
+                  <td>
+                    <a href="editarreservah.php?id=<?php echo $mostrar ['id_reservacion']?>" class="btn bg-orange btn-flat margin fa fa-pencil"></a>
+                    <!-- hay una clase que se llama borrar esa se va a utilizar en un archivo JS -->
+                    <a href="#" data-id="<?php echo $mostrar ['id_reservacion'];?>" data-tipo="reservah" class="btn bg-maroon btn-flat margin borrar">
+                    <i class="fa fa-trash"></i>
+                    </a>
+                  </td>
 
-      <!-- Default box -->
-      <div class="box">
-        <div class="box-header with-border">
-          <h3 class="box-title">Hotel</h3>
-
+                </tr>
+                <?php } ?>
+                </tbody>
+                <tfoot>
+                <tr>
+                  <th>Cliente</th>
+                  <th>Fecha reservacion</th>
+                  <th>Fecha entrada</th>
+                  <th>Fecha salida</th>
+                  <th>accion</th>
+                </tr>
+                </tfoot>
+              </table>
+            </div>
+            <!-- /.box-body -->
+          </div>
+          <!-- /.box -->
         </div>
-        <div class="box-body">
-          <!--LLamar al formulario aqui-->
-
-          <?php
-          
-          include_once('../../controlador/ctr.plantilla.php');
-
-          ?>
-        </div>
-        <!-- /.box-body -->
-        <div class="box-footer">
-          Footer
-        </div>
-        <!-- /.box-footer-->
+        <!-- /.col -->
       </div>
-      <!-- /.box -->
-
+      <!-- /.row -->
     </section>
     <!-- /.content -->
   </div>
-</main>
+
+  <!-- Control Sidebar -->
+  
+  <!-- /.control-sidebar -->
+  <!-- Add the sidebar's background. This div must be placed
+       immediately after the control sidebar -->
+  
+<!-- ./wrapper -->
