aaac7db0e00c0809ae942b88ccc6f89e5740361e
maikyd12 <maikyd12@gmail.com>
2021-08-15 08:20:51 -0600

    cambios maiky

--- controlador/ctr.boleteria.php
+++ controlador/ctr.boleteria.php
@@ -37,10 +37,9 @@ if(isset($_POST['action']) == 'registrartickets'){
                         }
                             if($id_cobrado){      
                                 
-                                        //inserta en la tabla tbl_boletos_detalle por boletos adultos nacional vendidos                               
-                                        $nacionalidad=1;
-                                        $insert=$conn->prepare("INSERT INTO tbl_boletos_detalle (cantidad_boletos, sub_total, tipo_nacionalidad_id, usuario_id, tipo_boleto_id, localidad_id, boletos_vendidos_id,
-                                                            estado_eliminado, creado_por, fecha_creacion, modificado_por, fecha_modificacion) VALUES(?,?,?,?,?,?,?,?,?,?,?,?)");
+                                        //inserta en la tabla tbl_boletos_detalle por boletos adultos nacional vendidos 
+                                        $insert=$conn->prepare("INSERT INTO tbl_boletos_detalle (cantidad_boletos, sub_total, usuario_id, tipo_boleto_id, localidad_id, boletos_vendidos_id,
+                                                            estado_eliminado, creado_por, fecha_creacion, modificado_por, fecha_modificacion) VALUES(?,?,?,?,?,?,?,?,?,?,?)");
                                                             foreach($conte as $valor){
                                                                 $cantidad = $valor->CantBoleto;
                                                                 $TipoBoletoS = $valor->TipoBoletoS;
@@ -48,7 +47,7 @@ if(isset($_POST['action']) == 'registrartickets'){
                                                                 $subtotal = $valor->totalPB;
                                                                 
                                                             
-                                        $insert->bind_param('iiiiiiiissss',$cantidad, $subtotal, $nacionalidad, $id_usuario, $TipoBoletoS, $Localidad, $idbv, $estado, $usuario_actual, $fecha, $usuario_actual, $fecha);
+                                        $insert->bind_param('iiiiiiissss',$cantidad, $subtotal, $id_usuario, $TipoBoletoS, $Localidad, $idbv, $estado, $usuario_actual, $fecha, $usuario_actual, $fecha);
                                         $insert->execute();    
                                                             }
                                                  }      
@@ -83,94 +82,3 @@ if(isset($_POST['action']) == 'registrartickets'){
     }
     echo json_encode($respuesta);
 }
-
-$res = array('error' => false);
-$action = '';
-
-if (isset($_GET['action'])) {
-    $action = $_GET['action'];
-}
-
-switch ($action){     
-   
-    case 'eliminarBoleto':
-        if (isset($_POST['id_boletos_vendidos'])) {            
-            $id_boletos_vendidos = $_POST['id_boletos_vendidos'];     
-            $user_id = $_POST['id_usuario'];
-            $estado_eliminar = 0;
-            $usuario_actual = $_POST['usuario_actual'];
-            $fecha = date('Y-m-d H:i:s', time());
-
-            $sql = "UPDATE tbl_boletos SET estado_eliminado = $estado_eliminar, modificado_por='$usuario_actual', fecha_modificacion='$fecha' WHERE id_boletos_vendidos = " . $id_boletos_vendidos;
-            $resultado = $conn->query($sql);
-            
-            if ($resultado == 1) {                 
-               
-                 //INSERTAR LA ACCION EN BITACORA
-                date_default_timezone_set("America/Tegucigalpa");
-                     $fecha2=date('Y-m-d H:i:s',time());
-                     $objeto = 7;
-                    $acciones = "Eliminación de Boletos";
-                    $descp = "Se ha eliminado la Orden de Venta No ". $id_boletos_vendidos;
-                    require_once("../modelo/conexionbd.php");
-                    $llamar1 = $conn->prepare("CALL control_bitacora (?, ?, ?, ?, ?);");
-                    $llamar1->bind_Param("sssii", $acciones, $descp, $fecha2, $user_id, $objeto);
-                    $llamar1->execute();
-                    $llamar1->close();    
-                    $res['msj'] = "Factura de Boleto(s) Eliminado(s)  Correctamente";  
-            } else {
-               
-                $res['msj'] = "Se produjo un error al momento de eliminar la factura";
-                $res['error'] = true;                
-            }
-                
-        } else {
-            $res['msj'] = "No se envió el id de la Factura a eliminar";
-            $res['error'] = true;
-        }
-        
-    break;   
-
-    case 'traerDetallesB':
-        $factura = $_GET['idbolvendido'];
-        try {
-
-            $sql =  "SELECT cantidad_boletos, sub_total, tbl_tipo_boletos.nombre_tipo_boleto, tbl_tipo_boletos.precio_venta, tbl_tipo_boletos.descripcion
-            FROM tbl_boletos_detalle            
-            INNER JOIN tbl_tipo_boletos
-            ON tbl_boletos_detalle.tipo_boleto_id=tbl_tipo_boletos.id_tipo_boleto
-            INNER JOIN tbl_boletos
-            ON tbl_boletos_detalle.boletos_vendidos_id=tbl_boletos.id_boletos_vendidos           
-            WHERE tbl_boletos.id_boletos_vendidos = $factura";                           
-            
-            $result = $conn->query($sql);
-        } catch (\Exception $e) {
-            echo $e->getMessage();
-        }
-
-        $vertbl = array();
-        while ($eventos = $result->fetch_assoc()) {
-            $evento = array(               
-                'cantidad' => $eventos['cantidad_boletos'],
-                'NombreBoleto' => $eventos['nombre_tipo_boleto'],
-                'precio' => $eventos['precio_venta'],
-                'subtotal' => $eventos['sub_total'],
-                'moneda' => $eventos['descripcion'],
-               
-                
-            );
-            array_push($vertbl, $evento);
-        }
-        $res['boletos'] = $vertbl;
-
-        break;
-    
-    default:
-        echo "Falló";
-    break;
-}
-$conn->close();
-header('Content-Type: application/json'); 
-//header("Location: ../senderos?signup=success");
-//echo $res['solicitudes'][0]['NombrePractica'];
-echo json_encode($res);
